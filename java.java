849c0dd62a99c574ce66b41a1692d90539f62093
huya111 <54341446+huya111@users.noreply.github.com>
2019-08-24 11:01:29 +0800

    云平台登陆代码
    dome2账号密码登录，

--- java.java
+++ java.java
@@ -1,3 +1,34 @@
-swth
-if
-char
+	public void initListener() {
+		mBtnLoght.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				mBtnLoght.setEnabled(false);
+				ShowProgressDialog("登录中");
+				String userName = mEtName.getText().toString().trim();
+				String passWord= mEtWrod.getText().toString().trim();
+				HttpEngine.SERVER_URL="http://192.168.15.16:80";
+				mAppAction.login(userName, passWord, "text", new ActionCallbackListener<AccessToken>() {
+					
+					@Override
+					public void onSuccess(AccessToken data) {
+						// TODO Auto-generated method stub
+						Toast.makeText(MainActivity.this, "AccessToaken:"+data.getAccessToken(), Toast.LENGTH_SHORT).show();
+						HttpEngine.ACCESSTOKEN=data.getAccessToken();
+						DismissProgressDialog();
+						startActivity(new Intent(MainActivity.this,HomeActivity.class));
+						MainActivity.this.finish();
+					}
+					
+					@Override
+					public void onFailure(String errorEven, String message) {
+						// TODO Auto-generated method stub
+						DismissProgressDialog();
+						ShowToast("错误信息"+message);
+						mBtnLoght.setEnabled(true);
+					}
+				});
+				
+			}
+		});
+	}
